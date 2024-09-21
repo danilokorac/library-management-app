6fe58b0dd4f149343ca0ab50ef0a73534246a704
Danilo Korać <72507305+danilokorac@users.noreply.github.com>
2024-09-21 23:12:43 +0200

    Implemented create, update and delete for user membership type

--- library-management-be/src/main/java/com/project/library_management_be/controller/UserController.java
+++ library-management-be/src/main/java/com/project/library_management_be/controller/UserController.java
@@ -1,5 +1,6 @@
 package com.project.library_management_be.controller;
 
+import com.project.library_management_be.model.MembershipType;
 import com.project.library_management_be.model.User;
 import com.project.library_management_be.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,7 +16,7 @@ public class UserController {
 
     @PostMapping("/addUser")
     public ResponseEntity<User> addNewUser(@RequestBody User user) {
-        User newUser =  userService.addNewUser(user);
+        User newUser = userService.addNewUser(user);
         return new ResponseEntity<>(newUser, HttpStatus.CREATED);
     }
 
@@ -24,9 +25,28 @@ public class UserController {
         User updatedUser = userService.updateUser(user, userId);
         return new ResponseEntity<>(updatedUser, HttpStatus.OK);
     }
+
     @DeleteMapping("/deleteUser")
     public ResponseEntity<Void> deleteUser(@RequestParam(name = "id") Long userId) {
         userService.deleteUser(userId);
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+
+    public ResponseEntity<User> addUserMembership(@RequestParam(name = "id") Long userId,
+                                                  @RequestParam(name = "membership_type") MembershipType membershipType) {
+        User updatedUser = userService.addUserMembership(userId, membershipType);
+        return new ResponseEntity<>(updatedUser, HttpStatus.OK);
+    }
+
+    @PatchMapping("/updateUserMembership")
+    public ResponseEntity<User> updateUserMembership(@RequestParam(name = "id") Long userId,
+                                                     @RequestParam(name = "membership_type") MembershipType membershipType) {
+        User updatedUser = userService.updateUserMemberShip(userId, membershipType);
+        return new ResponseEntity<>(updatedUser, HttpStatus.OK);
+    }
+
+    public ResponseEntity<User> deleteUserMembership(@RequestParam(name = "id") Long userId) {
+        User updatedUser = userService.deleteUserMembership(userId);
+        return new ResponseEntity<>(updatedUser, HttpStatus.OK);
     }
 }
--- library-management-be/src/main/java/com/project/library_management_be/service/UserService.java
+++ library-management-be/src/main/java/com/project/library_management_be/service/UserService.java
@@ -1,9 +1,13 @@
 package com.project.library_management_be.service;
 
+import com.project.library_management_be.model.MembershipType;
+import com.project.library_management_be.model.Role;
 import com.project.library_management_be.model.User;
 import com.project.library_management_be.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.lang.reflect.Member;
 
 @Service
 public class UserService {
@@ -30,5 +34,29 @@ public class UserService {
 
     public void deleteUser(Long userId) {
         userRepository.deleteById(userId);
+    }
+
+    public User addUserMembership(Long userId, MembershipType membershipType) {
+        return userRepository.findById(userId).map(user -> {
+            if(user.getMembershipType() != null) {
+                throw new RuntimeException("User already have existing membership!");
+            }
+            user.setMembershipType(membershipType);
+            return userRepository.save(user);
+        }).orElseThrow(() -> new RuntimeException("User with id: " + userId + " does not exist in the database!"));
+    }
+
+    public User updateUserMemberShip(Long userId, MembershipType membershipType) {
+        return userRepository.findById(userId).map(user -> {
+            user.setMembershipType(membershipType);
+            return userRepository.save(user);
+        }).orElseThrow(() -> new RuntimeException("User with id: " + userId + " does not exist in the database!"));
+    }
+
+    public User deleteUserMembership(Long userId) {
+        return userRepository.findById(userId).map(user -> {
+            user.setMembershipType(null);
+            return userRepository.save(user);
+        }).orElseThrow(() -> new RuntimeException("User with id: " + userId + " does not exist in the database!"));
     }
 }
